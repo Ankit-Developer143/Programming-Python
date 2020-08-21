f672fb440fc358dc2f18325a662da56925769ce7
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-21 20:44:39 +0530

    Creating list

---
+++ list/SomeBasic .js
@@ -0,0 +1,15 @@
+mylist = [1, 5, 4, 6, 4, 8, 9, 8, 7]
+
+
+""" Sorts """
+mylist.sort()
+#op:-[1, 4, 4, 5, 6, 7, 8, 8, 9]
+
+"""Access Values"""
+
+result = mylist[2]
+#op:- 4
+
+
+print(result)
+print(mylist)
