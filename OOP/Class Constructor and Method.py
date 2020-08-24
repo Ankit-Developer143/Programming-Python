a763c6bb36c9f4f3bf6cc0834c7dcaaa1ecf7fe8
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-24 23:32:23 +0530

    Class constructor and Method" " "" "

---
+++ OOP/Class Constructor and Method.py
@@ -0,0 +1,14 @@
+class Circle():
+    pi = 3.14
+    
+    def __init__(self,radius = 1):
+        self.radius = radius
+        
+    def area(self):
+        return self.radius*self.radius*self.pi
+    
+    
+    
+result = Circle(3)
+print(result.area())
+        
