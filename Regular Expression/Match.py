63027ce7b876c150e35f0f07173ea40194598b47
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-26 21:55:57 +0530

    Regular Expression Match Search findall

---
+++ Regular Expression/Match.py
@@ -0,0 +1,29 @@
+""" After you've defined a regular expression, 
+the re.match function can be used to determine whether it matches at the beginning of a string. 
+
+To avoid any confusion while working with regular expressions, we would use raw strings as r"expression".
+"""
+
+import re
+
+pattern = r"spam"
+
+if re.match(pattern,"spamspamspam"):
+    print("Match")
+else:
+    print("Not Match")
+
+
+#op:- Match
+
+
+""" re.search The function re.search finds a match of a pattern anywhere in the string. """
+
+patterns = r"spam"
+
+if re.search(patterns,"iffispamfh9fhh"):
+    print("Match")
+else:
+    print("Not Match")
+    
+#Match
