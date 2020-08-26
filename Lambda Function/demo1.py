fe1247b1794d37a58dde7921ba608b5a7a4e1ed3
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-26 19:08:30 +0530

    Lambda Function

---
+++ Lambda Function/demo1.py
@@ -0,0 +1,20 @@
+#lambda
+
+# lambda arguments : expression
+
+x = lambda a:a+10
+print(x(10)) #op:-20
+
+x = lambda a,b: a+b
+print(x(2,2))
+#op:-4
+
+def add(n):
+    return lambda a: a*2
+
+doubler = add(2)
+print(doubler(3))
+#op:-6
+
+
+
