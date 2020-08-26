2a215d732d61640bd6752f3b2af22e91a596193f
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-26 18:46:46 +0530

    list Comprehension

---
+++ list Comprehension/Memory Error Demo2.py
@@ -0,0 +1,11 @@
+# even = [2*i for i in range(10**100)]
+# print(even)
+
+#big op:- memoryError
+
+
+a = [x*10  for x in range (5,9)]
+print(a)
+
+#op:-[50, 60, 70, 80]
+ 
---
+++ list Comprehension/demo1.py
@@ -0,0 +1,19 @@
+square = [i**2 for i in range(10)]
+print(square)
+
+# op:- [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+
+nums = [i*2 for i in range(10)]
+print(nums)
+
+#op:- [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+
+
+"""print Even"""
+
+evens = [i for i in range (10) if i%2 == 0 ]
+print(evens)
+
+#op:- i iterate :- [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#op:- even number [0, 2, 4, 6, 8]
