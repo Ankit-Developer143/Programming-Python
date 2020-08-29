24fdf88ad674944a8ee0b343a888a2266363e670
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-29 15:54:32 +0530

    onoff Python

--- onoff.py
+++ onoff.py
@@ -1,5 +1,27 @@
 def pos_com(num):
     return pow(2,num)
 
-print(pos_com(10))
-#op:-1024
+print(pos_com(10)) 
+# op:-1024
+
+
+
+
+
+
+def is_odd(num):
+    if num % 2 == 1:
+        return True
+    else:
+       return False
+print(is_odd(1))
+
+
+
+
+#Divide Evenely
+def divides_evenly(a, b):
+   return a%b == 0
+
+print(divides_evenly(4,2))
+#op:-True
