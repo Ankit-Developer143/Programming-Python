4236d5ff45c010ae8655e4300a25b9c84ffb4551
Ankit-Developer143 <dante7785@gmail.com>
2020-09-03 19:35:10 +0530

    Inheritance

---
+++ OOP/Inheritance/Inheritance.py
@@ -0,0 +1,28 @@
+class Animal:
+    def __init__(self):
+        print("Animal Created")
+
+    def whoAmI(self):
+        print("Animal")
+
+    def eat(self):
+        print("Animal Eat")
+
+
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print("Dog Created")
+
+
+result = Dog()
+result.whoAmI()
+result.eat()
+
+
+""" 
+Animal Created
+Dog Created
+Animal
+Animal Eat
+ """
